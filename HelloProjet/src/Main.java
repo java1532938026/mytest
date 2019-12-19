95883690ca050552395a6018e5dfa68643a59652
Git <1532938026@qq.com>
2019-12-19 16:08:47 +0800

    修改hello

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -9,4 +9,10 @@ public class Main {
 	public static void main (String[] args){
 		System.out.println("HelloWorld!");
 	}
+	public static void main (String[] args){
+		System.out.println("12313");
+	}
+	public static void main (String[] args){
+		System.out.println("4123131");
+	}
 }
